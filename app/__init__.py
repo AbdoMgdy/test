1c875fb7be085108c7ff2687e1c4fef11fe61308
AbdoMgdy <abdoman46@hotmail.com>
2020-12-16 20:30:33 +0200

    add cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, request, jsonify
 from flask_restx import Api, Resource
+from flask_cors import CORS
 from .detect import get_coordinates
 
 
@@ -8,6 +9,7 @@ def create_app(env=None):
     from app.config import config_by_name
     template_dir = os.path.abspath('./app/templates')
     app = Flask(__name__, template_folder=template_dir)
+    CORS(app)
     app.config.from_object(config_by_name[env or "test"])
     api = Api(app, title="Test API", version="0.1.0")
 
@@ -15,17 +17,17 @@ def create_app(env=None):
     def index():
         return render_template('index.html')
 
-    @api.route("/test")
+    @api.route("/upload")
     class Video(Resource):
         def get(self):
             return {'hello': 'world'}
 
         def post(self):
-            uploaded_file = request.files['file']
+            uploaded_file = request.form.get('file')
             print(uploaded_file)
             uploaded_file.save(uploaded_file.name)
             cords = get_coordinates(uploaded_file.name)
             print(cords)
-            return cords, 200
+            return 200
 
     return app
