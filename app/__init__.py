2d5450a0904199e23b4477a17df7893e2ce21ac6
AbdoMgdy <abdoman46@hotmail.com>
2021-01-17 16:20:37 +0200

    test

--- app/__init__.py
+++ app/__init__.py
@@ -29,7 +29,7 @@ def create_app(env=None):
             print(uploaded_file)
             uploaded_file.save(uploaded_file.name)
             cords = getRes(uploaded_file.name)
-            j_cords = json.dumps(cords.toList())
+            j_cords = json.dumps(cords)
             print(j_cords)
             return j_cords, 200
 
